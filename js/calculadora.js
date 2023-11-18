da14152b78dbd48ce7b18e1a18a2b33cbbe5d7bc
Leonardo Gironde de Gusmão <95931762+leoggusmao@users.noreply.github.com>
2023-11-17 21:43:27 -0300

    Update calculadora.js

--- js/calculadora.js
+++ js/calculadora.js
@@ -12,12 +12,18 @@ function voltar(){
     document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
 }
 
-function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado)
-    }
-    else{
-        document.getElementById('resultado').innerHTML = "Impossivel calcular"
+function calcular() {
+    var resultadoElement = document.getElementById('resultado');
+    var resultado = resultadoElement.innerHTML.trim();
+    
+    /* verifica se o ultimo caractere é um numero*/
+    var ultimoCaractereNumerico = /\d$/. test(resultado);
+    /essa é uma expressão nativa do javascript: onde /\d/ é uma classe de caracteres que corresponde a qualquer dígito decimal (0-9) e $: Isso representa o final de uma string./ 
+    
+    if (ultimoCaractereNumerico) {
+        resultadoElement.innerHTML = eval(resultado).toFixed(2); /* eval faz o calculo na forma de uma expressão Ex: 1455+5-10 o resultado sera = 1450 tofixed(2) trava em 2 casas decimais*/
+    } else {
+        alert("Mensagem de erro: Impossível Calcular"); /* a função alert chama uma pop up com a mensagem de erro*/
+        limpar(); /* Chama a função limpar*/
     }
 }
